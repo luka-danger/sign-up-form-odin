468d01d93e513913378c0a92b2a21e266e80ef69
luka-danger <lukathevegan@gmail.com>
2024-02-10 00:28:08 -0700

    Move javascript to inline

--- password-check.js
+++
@@ -1,7 +0,0 @@
-// Check if passwords match 
-function onChange() {
-    let password = document.querySelector('input[name=password');
-    let confirm = document.querySelector('input[name=confirm');
-    (confirm.value === password.value) 
-    ? confirm.setCustomValidity('') : confirm.setCustomValidity('Password does not match');
-}
