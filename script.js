02d6af35dc0cfbaddfa9ff77d1a5c5109890a379
luka-danger <lukathevegan@gmail.com>
2024-02-10 00:24:11 -0700

    Create function to check if passwords match

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ submit.addEventListener('click', () => {
     alert("This site is still under construction! Thank you for your patience!")
 });
 
+// Check if passwords match 
+function onChange() {
+    let password = document.querySelector('input[name=password');
+    let confirm = document.querySelector('input[name=confirm');
+    (confirm.value === password.value) 
+    ? confirm.setCustomValidity('') : confirm.setCustomValidity('Password does not match');
+}
+
 
 
 
